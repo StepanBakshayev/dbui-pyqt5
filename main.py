be3b4ab10958ab7a8ef01b9b6818aa82d0a7c40c
Stepan Bakshayev <step2back@gmail.com>
2020-10-11 16:55:22 +0700

    concoct standalone async loop

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQuick import QQuickView
 from PyQt5.QtQml import QQmlApplicationEngine
 
+from threading import Thread
 from pathlib import Path
 import sys
 
@@ -29,7 +30,25 @@ class ApplicationState(QObject):
         self.url_ = url
 
 
+async def login_process():
+    from asyncio import sleep
+    import sys
+    while True:
+        print('.', end='')
+        sys.stdout.flush()
+        await sleep(1)
+
+
+def application_live():
+    from asyncio import new_event_loop
+    loop = new_event_loop()
+    loop.run_until_complete(login_process())
+
+
 if __name__ == '__main__':
+    live = Thread(name='live', target=application_live)
+    live.start()
+
     QGuiApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
 
     root = Path(__file__).absolute().parent
@@ -37,7 +56,7 @@ if __name__ == '__main__':
     application_state = ApplicationState(url='postgres://')
     application_state.connectDB.connect(lambda url: print('connecting...', url))
 
-    app = QGuiApplication(sys.argv)
+    application = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
     context = engine.rootContext()
     context.setContextProperty('application_state', application_state)
@@ -45,4 +64,4 @@ if __name__ == '__main__':
     window = engine.rootObjects()[0]
     window.show()
 
-    sys.exit(app.exec_())
+    sys.exit(application.exec_())
